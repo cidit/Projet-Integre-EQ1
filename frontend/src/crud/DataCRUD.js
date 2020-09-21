f5ce9b75f536af88278c3cf472958443953a7dc0
felix st-gelais <ciditworld@gmail.com>
2020-09-21 09:38:40 -0400

    EQ1-1-FRONTEND
    made a simplified fetch function, as well as a student model and a register component for the student but im having trouble wrapping my head around state transfers in react so i havent yet implemented the transfer of data between the front end and the backend.
    also, i've setup a proxy on port 8080.

---
+++ frontend/src/crud/DataCRUD.js
@@ -0,0 +1,5 @@
+
+export async function simpleFetch(url = "", method = "GET", data = {}) {
+    const response = await fetch(url, {method: method, body: JSON.stringify(data)})
+    return response.json()
+}
---
+++ frontend/src/model/Etudiant.js
@@ -0,0 +1,13 @@
+export default class Etudiant {
+    id;
+    matricule = "";
+    password = "";
+    nom = "";
+    prenom = "";
+    programme = "";
+    email = "";
+    telephone = "";
+    adresse = "";
+    statusStage = "";
+    stage;
+}
