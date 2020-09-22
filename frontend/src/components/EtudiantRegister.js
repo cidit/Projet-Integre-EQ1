0df1bf13b6e9da21227e98512dfa9a252fbff8e1
felix st-gelais <ciditworld@gmail.com>
2020-09-22 18:23:34 -0400

    EQ1-01
    refactoring

--- frontend/src/components/EtudiantRegister.js
+++ frontend/src/components/EtudiantRegister.js
@@ -18,7 +18,7 @@ export default class EtudiantRegister extends Component {
     handleSubmit(event) {
         event.preventDefault()
         simpleFetch("/api/create", "POST", this.state).then(r => console.log(r))
-        // this.props.onSubmitted(this.state)
+
     }
 
     render() {
@@ -26,22 +26,53 @@ export default class EtudiantRegister extends Component {
             <div className="container">
                 <h3>Register Etudiant</h3>
                 <form onSubmit={this.handleSubmit} className="d-flex flex-column">
-                    <label>Nom: <input className="form-control" type="text" name="nom" value={this.state.nom}
+                    <label>Nom: <input required="true"
+                                       className="form-control"
+                                       type="text"
+                                       name="nom"
+                                       value={this.state.nom}
                                        onChange={this.handleChange}/></label>
-                    <label>Prenom: <input className="form-control" type="text" name="prenom" value={this.state.prenom}
+                    <label>Prenom: <input required="true"
+                                          className="form-control"
+                                          type="text"
+                                          name="prenom"
+                                          value={this.state.prenom}
                                           onChange={this.handleChange}/></label>
-                    <label>Matricule: <input className="form-control" type="text" name="matricule"
-                                             value={this.state.matricule} onChange={this.handleChange}/></label>
-                    <label>email: <input className="form-control" type="text" name="email" value={this.state.email}
+                    <label>Matricule: <input required="true"
+                                             className="form-control"
+                                             type="text"
+                                             name="matricule"
+                                             value={this.state.matricule}
+                                             onChange={this.handleChange}/></label>
+                    <label>email: <input required="true"
+                                         className="form-control"
+                                         type="email"
+                                         name="email"
+                                         value={this.state.email}
                                          onChange={this.handleChange}/></label>
-                    <label>Password: <input className="form-control" type="text" name="password"
-                                            value={this.state.password} onChange={this.handleChange}/></label>
-                    <label>Telephone: <input className="form-control" type="text" name="telephone"
-                                             value={this.state.telephone} onChange={this.handleChange}/></label>
-                    <label>Adresse: <input className="form-control" type="text" name="adresse"
-                                           value={this.state.adresse} onChange={this.handleChange}/></label>
-                    <label>Programme: <input className="form-control" type="text" name="programme"
-                                             value={this.state.programme} onChange={this.handleChange}/></label>
+                    <label>Password: <input required="true"
+                                            className="form-control"
+                                            type="password"
+                                            name="password"
+                                            value={this.state.password}
+                                            onChange={this.handleChange}/></label>
+                    <label>Telephone: <input required="true"
+                                             className="form-control"
+                                             type="text" name="telephone"
+                                             value={this.state.telephone}
+                                             onChange={this.handleChange}/></label>
+                    <label>Adresse: <input required="true"
+                                           className="form-control"
+                                           type="text"
+                                           name="adresse"
+                                           value={this.state.adresse}
+                                           onChange={this.handleChange}/></label>
+                    <label>Programme: <input required="true"
+                                             className="form-control"
+                                             type="text"
+                                             name="programme"
+                                             value={this.state.programme}
+                                             onChange={this.handleChange}/></label>
                     <input className="btn btn-primary" type="submit" value="Submit"/>
                 </form>
             </div>
