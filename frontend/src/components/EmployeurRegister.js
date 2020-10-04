24ec23e4e064755efccf9298486acc6e6774c125
carlosortiz19 <carlos.arturo.ortiz@hotmail.com>
2020-10-04 15:26:48 -0400

    valider formulaire isa

--- frontend/src/components/EmployeurRegister.js
+++ frontend/src/components/EmployeurRegister.js
@@ -8,103 +8,121 @@ import { Formik, Field, Form, ErrorMessage, withFormik } from "formik";
 import * as Yup from 'yup';
 
 let redirectStr = "";
+
+const formSchema = Yup.object().shape({
+    nomEntreprise: Yup.string().required('Not empty'),
+    email: Yup.string()
+        .required('Not empty')
+        .email("Courriel inavalide"),
+    password: Yup.string()
+        .required("Not empty")
+        .min(5, "MInimun 5 caraters"),
+    telephone: Yup.string().required('Not empty').min(8, "minimo 8 caracteres"),
+    adresse: Yup.string().required('Not empty'),
+
+});
+
 export default class EmployeurRegister extends Component {
     constructor(props) {
         super(props);
         this.state = new Employeur();
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-        console.log(redirectStr);
-    }
-
-    async handleSubmit(event) {
-        event.preventDefault();
-        let x = "email";
-        let data = await EmployeurService.getByEmail(this.state[x]);
-        if (data[x] != this.state[x]) {
-            await EmployeurService.post(this.state);
-            redirectStr = "HOME";
-            this.forceUpdate();
-        } else {
-            alert("Ce email est deja utilise");
-        }
-    }
+    /* handleChange(event) {
+         this.setState({ [event.target.name]: event.target.value });
+         console.log(redirectStr);
+     }
+ 
+     async handleSubmit(event) {
+         event.preventDefault();
+         let x = "email";
+         let data = await EmployeurService.getByEmail(this.state[x]);
+         if (data[x] != this.state[x]) {
+             await EmployeurService.post(this.state);
+             redirectStr = "HOME";
+             this.forceUpdate();
+         } else {
+             alert("Ce email est deja utilise");
+         }
+     }*/
 
     render() {
-         const formSchema = Yup.object().shape({
-            nomEntreprise: Yup.string().required('Not empty'),
-            email: Yup.string().required('Not empty'),
-            password: Yup.string().required('Not empty'),
-            telephone: Yup.string().required('Not empty'),
-            adresse: Yup.string().required('Not empty'),
 
-         });
+
+        const { handleSubmit, isSubmitting, isValid, isValidating } = this.props;
         if (redirectStr == "HOME") {
             return <Redirect to={"/"} />
         } else {
             return (
+                <div className="container">
+                    <div className="card-body" >
+                        <h5 className="card-title text-center">Enregistrer nouveau employeur</h5>
+                        <Formik
+                            initialValues={{
+                                nomEntreprise: "",
+                                email: "",
+                                password: "",
+                                telephone: "",
+                                adresse: "",
+                            }}
+                            validationSchema={formSchema}
 
-                <div className="card p-3">
+                            onSubmit={({ setSubmitting }) => {
+                                alert("Form is validated! Submitting the form...");
+                                setSubmitting(false);
+                            }}
+                        >
 
-                    <Formik
-                        initialValues={{
-                            nomEntreprise: "",
-                            email: "",
-                            password: "",
-                            telephone: "",
-                            adresse: "",
+                            {({ touched, errors, isSubmitting }) => (
+                                <Form>
 
-                        }}
-                        validationSchema={formSchema}
+                                    <div className="row">
+                                        <div className="form-label-group">
+                                            <label className="control-label">Nom </label>
+                                            <Field type="text" name="nomEntreprise" className="form-control" />
+                                            <ErrorMessage name="nomEntreprise">{msg => <div>{msg}</div>}</ErrorMessage >
+                                        </div>
+                                    </div>
+                                    <label>
+                                        Email:
+                            <Field type="email" name="email" />
+                                        <ErrorMessage name="email">{msg => <div>{msg}</div>}</ErrorMessage >
+                                    </label>
+                                    <label>
+                                        Telephone:
+                            <Field type="tel" name="telephone" />
+                                        <ErrorMessage name="telephone">{msg => <div>{msg}</div>}</ErrorMessage >
+                                    </label>
+                                    <label>
+                                        Addresse:
+                            <Field type="text" name="adresse" />
+                                        <ErrorMessage name="adresse">{msg => <div>{msg}</div>}</ErrorMessage >
+                                    </label>
+                                    <label>
+                                        Mot de passe:
+                            <Field type="password" name="password" />
+                                        <ErrorMessage name="password">{msg => <div>{msg}</div>}</ErrorMessage >
+                                    </label>
+                                    <input type="submit" value="Register" />
 
-                        onSubmit={values => {
-                            // same shape as initial values
-                            console.log(values);
-                        }}
-
-                    >
 
 
-                    </Formik>
 
 
-                    <h5 className="breadcrumb ">Enregistrement Employeur</h5>
+                                </Form>
+                            )}
+                        </Formik>
 
-                    <Form >
-                        <div className="container">
-                            <div className="row">
-                                <div className="form-group col">
-                                    <label className="control-label">Nom </label>
-                                    <Field type="text" name="nom" className="form-control" required value={this.state.nom} onChange={this.handleChange} />
-                                </div>
-                            </div>
-                            <label>
-                                Email:
-                            <Field type="email" name="email" required value={this.state.email} onChange={this.handleChange} />
-                            </label>
-                            <label>
-                                Telephone:
-                            <Field type="tel" name="telephone" required value={this.state.telephone} onChange={this.handleChange} />
-                            </label>
-                            <label>
-                                Addresse:
-                            <Field type="text" name="adresse" required value={this.state.adresse} onChange={this.handleChange} />
-                            </label>
-                            <label>
-                                Mot de passe:
-                            <Field type="password" name="password" required value={this.state.password} onChange={this.handleChange} />
-                            </label>
-                            <input type="submit" value="Register" />
 
-                        </div>
 
-                    </Form>
 
+
+
+
+                    </div>
                 </div>
             );
         }
