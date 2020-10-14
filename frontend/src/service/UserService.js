b4ec5434143cdd1d422d78e0fb254c2b417139ca
carlosortiz19 <carlos.arturo.ortiz@hotmail.com>
2020-10-14 01:15:57 -0400

    back end fonctionnel et teste

--- frontend/src/service/UserService.js
+++ frontend/src/service/UserService.js
@@ -1,26 +1,14 @@
+import { number } from "yup";
 
 class UserService {
     async getByEmail(email) {
         let data;
         await fetch("http://localhost:8080/users/get/" + email, {method: "GET"})
-=======
-        await fetch("http://localhost:8080/users/get/richard@email.com", {method: "GET"})
-            .then(r=> console.log(r))
-            .then(r => data = r.json())
-            .catch(error => data = {});
+        .then(r => data = r.json())
+        .catch(error => data = {});
         return data;
-    }
-
-    async getByEmail2(email){
-        let data;
-        await fetch("http://localhost:8080/users/email?email=" +email, {method: "GET"} )
->>>>>>> testRichard
-            .then(r => data = r.json())
-            .catch(error => data = {});
-        return data;
-    }
+        }
 
 }
 
 export default new UserService()
-
