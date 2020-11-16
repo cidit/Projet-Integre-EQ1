c83531af34652f109a466116520a237fafd38042
felix st-gelais <ciditworld@gmail.com>
2020-11-16 15:52:35 -0500

    EQ1-133
    solved problem with hooks but having problems with javascript Promisses: need to obtain an async result in a function that cannot be static

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import {Container, List} from "@material-ui/core";
+import React, {useState} from 'react';
+import {Container, List, Button, Typography} from "@material-ui/core";
+import UserService from "../service/UserService";
 
-export default function Home(props) {
+export default async function Home(props) {
     if (props.location.search === "?refresh") {
         props.history.replace("/")
         window.location.reload(false);
     }
     const [userId, userDesc] = [localStorage.getItem("id"), localStorage.getItem("desc")];
     // const [reminders, setReminders] = useState(UserService.getReminders(userId))
+    let reminders = await UserService.getReminders(userId)
 
-    //let cards = []
+    // UserService.getReminders(userId)
+    //     .then(value => reminders = value)
+    //     .catch(reason => console.log("nope"))
 
+    console.log(typeof reminders)
+    // let cards = reminders.map(reminder => <Typography>{reminder}</Typography>)
+    console.log(reminders)
     return (
         <Container>
-            {/*<Button onClick={setReminders(UserService.getReminders(userId))}>Refresh</Button>*/}
+            {/*<Button onClick={() => setReminders(UserService.getReminders(userId))}>Refresh</Button>*/}
             <List>
-                {/*{reminders}*/}
+                {reminders}
             </List>
         </Container>
     )
--- frontend/src/service/UserService.js
+++ frontend/src/service/UserService.js
@@ -12,7 +12,9 @@ class UserService {
         await fetch("http://localhost:8080/users/reminders/" + id, {method: "GET"})
             .then(response => data = response.json())
             .catch(err => data = {})
+
         return data
+        //return fetch("http://localhost:8080/users/reminders/" + id, {method: "GET"})
     }
 }
 
