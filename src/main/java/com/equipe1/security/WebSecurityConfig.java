3de33f3dff6a1fa67c3badc60fb0922e0309e187
Richard Truong <richard.truong084@gmail.com>
2020-11-29 14:04:45 -0500

    ajout WebSecuirtyConfig h2 console

--- src/main/java/com/equipe1/security/WebSecurityConfig.java
+++ src/main/java/com/equipe1/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.headers().frameOptions().sameOrigin().and().cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
