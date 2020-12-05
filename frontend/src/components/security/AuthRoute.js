2b06051980ec6a4ed7a29dc3e9c60ead94664dd1
Richard Truong <richard.truong084@gmail.com>
2020-12-05 11:02:10 -0500

    authroute.js fix

--- frontend/src/components/security/AuthRoute.js
+++ frontend/src/components/security/AuthRoute.js
@@ -5,14 +5,17 @@ import AuthService from "../../service/security/auth.service";
 
 export default function Rappel(props) {
     const isAuth = !!AuthService.getCurrentUser();
-    const isExired = AuthService.verifyTokenExpired();
+    const isExpired = AuthService.verifyTokenExpired();
     const currentRole = AuthService.getTokenDESC();
     const hasRequiredRole = props.requiredRole.includes(currentRole);
 
-    if (isExired) {  
+    if (isExpired) {  
         AuthService.logout();
+        return (
+            <Redirect to='/login'/>
+        )
     }
-    if (isAuth && hasRequiredRole && !isExired) {  
+    if (isAuth && hasRequiredRole && !isExpired) {  
         return (
             <Route path={props.path} component={props.component}/>
         )
