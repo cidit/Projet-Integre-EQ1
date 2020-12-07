1aa3a10d39f84ae1bd9d854ad2dc9eb0cc9a936a
Richard Truong <richard.truong084@gmail.com>
2020-12-07 01:15:17 -0500

    removed loop in insertDataService

--- src/main/java/com/equipe1/service/InsertDataService.java
+++ src/main/java/com/equipe1/service/InsertDataService.java
@@ -473,27 +473,6 @@ public class InsertDataService {
         enseignant5.setRoles(roles);
 
         enseignantRepository.save(enseignant5);
-
-        for (int i = 0; i <30 ; i++) {
-            enseignant5 = new Enseignant();
-            enseignant5.setNom("Kristian "+ i);
-            enseignant5.setPrenom("Redman " + i);
-            //enseignant5.setPassword("123456" + i);
-            enseignant5.setPassword(encoder.encode("123456"));
-            enseignant5.setProgramme("Techniques de l’informatique");
-            enseignant5.setEmail("Kristian@email.com" +i);
-            enseignant5.setTelephone("4389522222");
-
-            roles = new HashSet<>();
-            role = roleRepository.findByName(Role.ERole.ROLE_ENSEIGNANT)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(role);
-
-            enseignant5.setRoles(roles);
-
-            enseignantRepository.save(enseignant5);
-        }
-
     }
 }
 
